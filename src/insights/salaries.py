b07b704fda098f2f321c5617624d031e3c799b22
Alex <alexsandrolima991@hotmail.com>
2023-03-24 20:35:11 -0300

    (feat): matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -25,34 +25,25 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        min_salary = int(job["min_salary"])
+        max_salary = int(job["max_salary"])
+        salary = int(salary)
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+        if min_salary > max_salary:
+            raise ValueError
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+        if min_salary is None or max_salary is None:
+            raise ValueError("Alguma das chaves estão ausentes")
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    except (Exception):
+        raise ValueError("Algumas das chaves não têm valores numéricos")
+
+    return min_salary <= int(salary) <= max_salary
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
 
