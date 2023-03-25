269fe24cbc6047bde211ec54693628897e5e0b39
Alex <alexsandrolima991@hotmail.com>
2023-03-24 22:53:00 -0300

    (tests): test para função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+    Javascript_counter = 122
+    python_counter = 1639
+
+    assert (
+        count_ocurrences("data/jobs.csv", "Javascript") == Javascript_counter
+    )
+
+    assert count_ocurrences("data/jobs.csv", "python") == python_counter
+
+    with pytest.raises(
+        AttributeError, match="'int' object has no attribute 'lower'"
+    ):
+        count_ocurrences("data/jobs.csv", 3)
